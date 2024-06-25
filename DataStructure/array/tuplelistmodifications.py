d1d4212f83d90d679b105688f7eec86eddc2a15d
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-25 19:05:09 +0530

    second

---
+++ DataStructure/array/tuplelistmodifications.py
@@ -0,0 +1,16 @@
+'''trying modifications'''
+
+myTuple=tuple([1,2,3])
+myTuple_=tuple([4,5,6])
+
+a=myTuple.__add__(myTuple_)
+print(a) #adding both tuples into one
+b= myTuple.__mul__(3) 
+print(b) #repeated concatenation
+c=myTuple.__rmul__(3)
+print(c) # reversed repeated concatenation 
+d= myTuple.__iter__()
+print(d) #get iterator
+
+
+
