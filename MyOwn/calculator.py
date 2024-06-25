727a8f16b3488c9ec282ab94b648fd8e0113e0ce
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-26 02:14:12 +0530

    update

---
+++ MyOwn/calculator.py
@@ -0,0 +1,12 @@
+''' the user can add as many as numbers they want to'''
+
+def addition():
+    myNumbers=[]
+    for i in range(5):
+     numbers=float(input("numbers: "))
+     myNumbers.append(numbers)
+    print(myNumbers)
+    
+
+
+    
