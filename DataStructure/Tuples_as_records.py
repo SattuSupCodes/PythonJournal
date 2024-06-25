4a6b2415849fecbef9335a467ba3d794dc007e66
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-25 18:19:57 +0530

    second

---
+++ DataStructure/Tuples_as_records.py
@@ -0,0 +1,5 @@
+# tuples are defined just as lists which are immutable, but they have one more duty; as record
+
+'''tuples hold records. Each item in the tuple holds the data for one field, and the
+position of the item gives its meaning'''
+
