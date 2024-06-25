062b9c26c0c7a93f4700540c051fd17748f164f9
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-26 00:07:17 +0530

    match-case

---
+++ DataStructure/array/MatchCase.py
@@ -0,0 +1,15 @@
+# match case in python
+# method from an imaginary robot class
+
+def handle_class(self,message):
+    match message:
+        case['BEEPER', frequency, times]:
+            self.beep(times, frequency)
+        case['NECK', angle]:
+            self.rotate_neck(angle)
+        case['LED', ident, intensity]:
+            self.leds[ident].set_brightness(ident, intensity)
+        case['LED', ident, red, green, blue]:
+            self.leds[ident].set_color(ident, red, green, blue)
+        case _:
+            raise InvalidCommand(message)
