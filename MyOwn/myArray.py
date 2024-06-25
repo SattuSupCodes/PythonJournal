c2914de408f40b56766b2708810f5d40fd9dad54
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-25 18:50:16 +0530

    second

---
+++ MyOwn/myArray.py
@@ -0,0 +1,16 @@
+# i want to create an array list using user inputs
+myArray=[]
+
+n=int(input("enter no. of elements: "))
+
+for i in range(0,n):
+    elements=int(input("enter your elements: "))
+    myArray.append(elements)
+    
+print(myArray)
+
+# python simply uses lists as array, although you can try the import array method. 
+
+import array as arr 
+a=arr.array('I', [1,2,3])
+print(a)
