53be951ebb8b86f428ee000340f9c2d4939e15b5
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-26 00:24:28 +0530

    operators

---
+++ lambdafunction.py
@@ -0,0 +1,10 @@
+'''lambda function have you take as many as arguments you want but lets you work with one 
+expression only'''
+
+# example 1
+
+a= lambda b,c : b+c
+print(a(5,6))
+# example 2
+g= lambda h,w,y,x: x*y*h*w
+print(g(3,4,5,6))
