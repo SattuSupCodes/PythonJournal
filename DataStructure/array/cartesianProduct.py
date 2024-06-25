6f2f3cca8975aefe46300941660d5f8923bca594
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-25 17:49:17 +0530

    first commit

---
+++ DataStructure/array/cartesianProduct.py
@@ -0,0 +1,10 @@
+# using 2D arrays we do cartesian product with listcomp
+colors=['black', 'white']
+sizes=['s', 'm', 'l']
+tshirts=[(colors, size) for color in colors for size in sizes]
+print(tshirts)
+
+# second method
+for color in colors:
+    for size in sizes:
+        print((color, size))
