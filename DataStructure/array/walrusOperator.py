c5caf207bfa8046de685f3014d9f015aa433e479
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-25 16:59:18 +0530

    first commit

---
+++ DataStructure/array/walrusOperator.py
@@ -0,0 +1,7 @@
+'''walrus operator := used when you to assign a value to a variable within an expression. 
+This can be useful when you need to use a value multiple times in a loop,
+but don't want to repeat the calculation. '''
+
+x="ABC"
+codes=[last:= ord(c) for c in x]
+print(codes)
