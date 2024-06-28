8d49e4d407db7dd759395750de09737c5c6a0100
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-28 16:34:19 +0530

    upd

---
+++ NumPy/Nm.py
@@ -0,0 +1,16 @@
+''' Numpy is a python library which comes in great use when dealing with advanced
+array and matrix operations. 
+
+NumPy is the reason python is now being used in scientific computing applications.
+
+NumPy implements multi-dimensional, homogeneous, and matrix types that hold not only numbers
+but also user-defined records, and provides efficient element-wise operations.
+
+SciPy is a liberary written on top of NumPy, providing many scientific computing
+algorithms from linear algebra, numerical calculusm and statistics. '''
+
+# example
+import NumPy as np
+a = np.arange(12)
+print(a)
+
