fad0560ef27f5ee02425b3937fe6d51fc6f36813
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-29 02:56:52 +0530

    upd

--- DataStructure/array/Queue.py
+++ DataStructure/array/Queue.py
@@ -1,6 +1,18 @@
-'''queue functions such as pop and dequeue. 
+'''queue functions such as pop and deque. 
 '''
 
 '''the .append and .pop methods make a list usable as a stack or a queue (we get FIFO behaviour
 upon using these)'''
+
+from collections import deque
+'''the class collections.deque  is a thread-safe double-ended queue designed for last inserting and removing from 
+both ends. When a deque is full, adding an item automatically discards an item on the
+opposite end. '''
+
+dq = deque(range(10), maxlen=10)
+print(dq)
+dq.appendleft(-1)
+print(dq)
+dq.extend([22,66])
+print(dq)
 
