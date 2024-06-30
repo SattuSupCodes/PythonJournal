aca84bfe3ef1fc02c2b0eab4fcbbaf4f9c6df093
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-30 21:03:38 +0530

    upd

---
+++ Beginning/chapter1/conditionalStatements.py
@@ -0,0 +1,10 @@
+a='how you doin?'
+b='sup?'
+print(a,b)
+choices=input("choose 'how you doin?' or 'sup?': ")
+if(choices==a):
+    print("you think you joey?")
+elif(choices==b):
+    print("srsly? sup? you could do better")
+else:
+    ("just choose from the options bruh")
