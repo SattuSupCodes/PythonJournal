4e3584dad4597ffaa5ce5afc0fe9d3fb367a7149
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-06-30 20:44:06 +0530

    upd

--- MyOwn/tictactoe.py
+++ MyOwn/tictactoe.py
@@ -1,5 +1,8 @@
 '''we will slowly build a tic tac toe game here as we keep learning python'''
 
+'''THIS IS A WORK IN PROGRESS
+I WILL MAKE THIS MYSELF STEP BY STEP'''
+
 # we first create a 1-D board
 
 board=[['_']*3 for i in range(3)]
@@ -7,5 +10,11 @@ print(board)
 board[1][2]='x'
 print(board)
 
+choice=input("choose: ")
+if(choice=='x'or 'X'):
+    print(choice)
+else:
+    print("hoehoe")
+    
 '''we need to make it look like a tic tac toe board now with user inputs'''
 '''we can use NumPy to create the matrix for the board 3x3'''
