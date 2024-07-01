2c0bcbac8a17ec9ce4a24390be164a33547e461b
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-07-01 21:27:07 +0530

    upd

---
+++ Beginning/chapter1/Modules.py
@@ -0,0 +1,9 @@
+from functions import *
+
+a=int(input("enter num1: "))
+b=int(input("enter num2: "))
+
+addition(a,b)
+substract(a,b)
+
+'''created a simple module functions.py then imported it here.'''
--- Beginning/chapter1/functions.py
+++ Beginning/chapter1/functions.py
@@ -1,1 +1,8 @@
 '''python functions'''
+def addition(num1,num2):
+    sum=num1+num2
+    print(sum)
+    
+def substract(num1,num2):
+    difference=num1-num2
+    print(difference)
