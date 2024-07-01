bfbc2df58d4b426aedf8a32ddae684a508ce5ca9
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-07-01 21:51:09 +0530

    upd

--- Beginning/chapter1/Modules.py
+++ Beginning/chapter1/Modules.py
@@ -1,4 +1,5 @@
 from functions import *
+from random import *
 
 a=int(input("enter num1: "))
 b=int(input("enter num2: "))
@@ -7,3 +8,9 @@ addition(a,b)
 substract(a,b)
 
 '''created a simple module functions.py then imported it here.'''
+
+random_no=randint(6,90)
+print(random_no)
+
+dice=randint(1,6)
+print(dice)
