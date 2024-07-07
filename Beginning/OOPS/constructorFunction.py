de5616e4106d4ab61aa0b7b492f890274f06e41f
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-07-07 19:45:51 +0530

    upd

--- Beginning/OOPS/constructorFunction.py
+++ Beginning/OOPS/constructorFunction.py
@@ -3,5 +3,5 @@ is created. It creates an instance of a specific class
 
 __init__  is the name we must
 use for the constructor funtion
-__init__ -> Initialze -> Start'''
+__init__ -> Initialze -> Start '''
 
