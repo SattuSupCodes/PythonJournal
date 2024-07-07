5e6043a483d9df39e879f87a2e4215b8574a86d0
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-07-07 18:08:02 +0530

    upd

---
+++ Beginning/OOPS/class.py
@@ -0,0 +1,3 @@
+'''A class contains the blueprints or the prototype from which the objects are being created. 
+It is a logical entity that contains some attributes and methods.'''
+
---
+++ Beginning/OOPS/object.py
@@ -0,0 +1,8 @@
+'''An object is a singular item in the same group, but each object
+has it's own properties and spceial characteristics'''
+
+'''each object has properties (variables) and are controlled by methods (functions)
+
+A method has to be linked by an object to work'''
+
+
