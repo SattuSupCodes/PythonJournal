aa901f4a12a1b70a98625d8a23318bb96afe6f77
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-07-07 18:28:23 +0530

    upd

---
+++ Beginning/OOPS/Turtle.py
@@ -0,0 +1,16 @@
+from turtle import *
+
+red_turtle= Turtle()
+red_turtle.color("red")
+
+purple_turtle=Turtle()
+purple_turtle.color("purple")
+
+#  property must be linked to objects to work
+
+red_turtle.speed=5
+red_turtle.shape("turtle")
+purple_turtle.forward(100)
+purple_turtle.speed=5
+red_turtle.left(120)
+red_turtle.forward(100)
