38c3557999152edb07bbfa87001ff034dfc978bf
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-07-18 21:15:56 +0530

    upd

--- MyOwn/hangman.py
+++ MyOwn/hangman.py
@@ -6,3 +6,12 @@ print("welcome to hangman")
 li=['pineapple', 'apple' , 'orange']
 
 print("choose your word", li)
+
+secret_word = random.choice(li)
+
+guess = input("Guess a letter").lower
+
+print(guess)
+
+
+    
