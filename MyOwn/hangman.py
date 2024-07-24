33057d6bc3f2b90f7186bd0842ddbda2bb21b5ef
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-07-24 20:06:11 +0530

    updates

--- MyOwn/hangman.py
+++ MyOwn/hangman.py
@@ -2,5 +2,26 @@
 
 import random
 from wordList import word_list
-# dictionary 
-hangman_art = {}
+# dictionary of key() 
+hangman_art = { 0: ("   ",
+                    "   ",
+                    "   "),
+                1: (" o ",
+                    "   ",
+                    "   "),
+                2: (" o ",
+                    " | ",
+                    "   "),
+                3: (" o ",
+                    "/| ",
+                     "   "),
+                4: (" o ",
+                    "/|\\",
+                    "   "),
+                5: (" o ",
+                    "/|\\",
+                    "/  "),
+                6: (" o ",
+                    "/|\\",
+                    "/ \\")}
+               
