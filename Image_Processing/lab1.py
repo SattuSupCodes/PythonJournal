87ef0f033e383c7474ab109e2d6c362e1faa7c51
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-09-26 10:05:07 +0530

    image processing

--- Image_Processing/lab1.py
+++ Image_Processing/lab1.py
@@ -1,2 +1,17 @@
 # working with openCV and i hope i succeed smh
+import cv2
+
+# Load the input image
+image = cv2.imread('Image_Processing/oh_my_bholenaath.jpeg')
+cv2.imshow('Original', image)
+cv2.waitKey(0)
+
+# Use the cvtColor() function to grayscale the image
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('Grayscale', gray_image)
+cv2.waitKey(0)  
+
+# Window shown waits for any key pressing event
+cv2.destroyAllWindows()
 
