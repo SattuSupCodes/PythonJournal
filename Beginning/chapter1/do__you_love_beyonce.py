ef59672735b974730ee36fc51efea54074858c79
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-10-16 04:00:14 +0530

    ALL HAIL BEYONCE

--- Beginning/chapter1/do__you_love_beyonce.py
+++ Beginning/chapter1/do__you_love_beyonce.py
@@ -1,12 +1,16 @@
 def beyonce():
     jayz = str(input("do you love Beyonce? (Y/N): "))
-    while jayz == 'N':
+    if jayz=='Y' or jayz =='y':
+        print("Good, you are safe lil nigga")
+    else:
+     while jayz == 'N' or jayz == 'n':
     
        print("you're lost, choose again")
        try_again_or_be_aaliyah = str(input("do you love beyonce?(Y,N): "))
        print(try_again_or_be_aaliyah)
        
-       if try_again_or_be_aaliyah == 'Y':
+       if try_again_or_be_aaliyah == 'Y' or jayz == 'y':
+           print('Good, you are safe lil nigga')
            break
        else:
            continue
@@ -14,3 +18,4 @@ def beyonce():
        
 beyonce()
        
+    #       i  is struggling with a minor inconvenience in this code but ik I'll figure it out hehehehehe 
