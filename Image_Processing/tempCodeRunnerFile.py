38071b8e35ab9ef2c90e0e2b5513eabbdc3fc293
Shatakshi Singh <161025110+SattuSupCodes@users.noreply.github.com>
2024-11-27 19:06:16 +0530

    streak

--- Image_Processing/tempCodeRunnerFile.py
+++ Image_Processing/tempCodeRunnerFile.py
@@ -1,25 +1,3 @@
-# transforming between images and methods for computing them
-
-# a Wrap affine tranformation
-
-import cv2 
-import numpy as np
-
-img = cv2.imread("image_processing/images/oh_my_bholenaath.jpeg",0)
-
-rows, cols = img.shape
-
-M = np.float32([[1,0,100], [0,1,50]])
-
-dst = cv2.warpAffine(img,M,(cols,rows))
-
-cv2.imshow('img', dst)
-cv2.waitKey(0)
-
-cv2.destroyAllWindows()
-
-# Affine transformation
-
 import cv2 
 import numpy as np
 import matplotlib as plt
